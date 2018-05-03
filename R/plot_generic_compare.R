59c76ec280718bfd98b56e5c242b9b99e295b0a4
James Thompson <james.thompson@imperial.ac.uk>
2018-05-03 13:40:15 +0100

    Allow the user to set disciminent to NULL to aggregate everything + fix comparing across years + fix label position

--- R/plot_generic_compare.R
+++ R/plot_generic_compare.R
@@ -14,10 +14,17 @@
 #' @export
 plot_generic_compare <- function(dat, params, compare, disciminent) {
   dat$country_name = sapply(dat$country_name, shorten_name, USE.NAMES = FALSE)
+  no_disc <- is.null(disciminent)
+  
+  if (no_disc) {
+    disciminent <- compare # we set this 
+  }
 
   dat$outcome <- dat[, params$metric]
   dat$compare <- dat[, compare]
   dat$disc    <- dat[, disciminent]
+  
+  ordered_compare <- is.numeric(dat$compare)
 
   dat <- filter_by_params(dat, params)
 
@@ -26,19 +33,30 @@ plot_generic_compare <- function(dat, params, compare, disciminent) {
     summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
     arrange(desc(outcome))
 
-  units <- graph_num_div(max(dat$outcome), params$metric)
-  dat$outcome <- dat$outcome / units$numdiv
-  top_label_shift <- max(dat$outcome) / (50 * units$numdiv) + 0.25
-  
   # find the top n_plot compares by outcome
-  df_top_compares <- dat %>%
-    group_by(compare) %>%
-    summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
-    arrange(desc(outcome))
+  if (ordered_compare) {
+    df_top_compares <- dat %>%
+      group_by(compare) %>%
+      summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
+      arrange(compare)
+  } else {
+    df_top_compares <- dat %>%
+      group_by(compare) %>%
+      summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
+      arrange(desc(outcome))   
+  }
+               
+  units <- graph_num_div(max(df_top_compares$outcome), params$metric)
+  dat$outcome <- dat$outcome / units$numdiv
+  df_top_compares$outcome <- df_top_compares$outcome / units$numdiv
+  top_label_shift <- max(df_top_compares$outcome) / 50
 
   top_compares <- df_top_compares$compare
-  if (params$n_plot < length(top_compares))
-    top_compares <- top_compares[1:params$n_plot]
+  if (!is.null(params$n_plot)) {
+    if (params$n_plot < length(top_compares)) {
+      top_compares <- top_compares[1:params$n_plot]
+    }
+  }
   
   df_top_compares <- df_top_compares %>%
     filter(compare %in% top_compares)
@@ -50,12 +68,10 @@ plot_generic_compare <- function(dat, params, compare, disciminent) {
     factor(dat$compare, levels = top_compares)
   
   my_cols <- generic_palette(unique(dat$disc))
-  
-  print(df_top_compares)
 
   dat$compare <- as.factor(dat$compare)
   df_top_compares$compare <- as.factor(df_top_compares$compare)
-  
+
   ggplot(dat, aes(x = compare,
                   y = outcome,
                   fill = factor(disc))) +
