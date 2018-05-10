7cd3b7f9f67e45ffc53ea73284bb5091761dbd65
James Thompson <james.thompson@imperial.ac.uk>
2018-05-10 17:08:32 +0100

    Extensive rewrite of power point functions to make it more usable

--- R/power_point.R
+++ R/power_point.R
@@ -1,14 +1,15 @@
 ################################################################################
 #' Creates a temporary Power Point object
 #' @param template_path The path to a pptx to use as a template
-#'
+#' @param title The title of the document in the file metadata
+#' 
 #' @return A temporary Power Point file
 #' @export
-create_powerpoint <- function(template_path=NULL) {
+create_powerpoint <- function(title, template_path=NULL) {
   if (!is.null(template_path)) {
-    return(pptx(template = template_path))
+    return(pptx(title = title, template = template_path))
   } else {
-    return(pptx())
+    return(pptx(title = title))
   }
 }
 
@@ -39,8 +40,11 @@ add_title_slide <- function(doc,
                             subtitle=NULL,
                             add_date=FALSE,
                             footer=NULL,
-                            page_number=NULL) {
-  doc <- addSlide(doc, "Title Slide")
+                            page_number=NULL,
+                            template_override = NULL) {
+  template_name <- if(is.null(template_override)) {"Title Slide"} else {template_override}
+  
+  doc <- addSlide(doc, template_name)
   doc <- addTitle(doc, title)
   if (!is.null(subtitle)) {
     doc <- addSubtitle(doc, subtitle)
@@ -58,9 +62,47 @@ add_title_slide <- function(doc,
 }
 
 ################################################################################
+#' Add an empty generic slide, a user can insert  user reportRs functions
+#' @param doc a Temporary Power Point file created by create_powerpoint()
+#' @param title The title of the slide
+#' @param cols The number of columns to add must be 1 or 2
+#' @param footer OPTIONAL a footer for the slide
+#' @param page_number OPTIONAL the page number
+#' 
+#' @return the temporary Power Point file with a text slide added
+#' @export
+add_generic_slide <- function(doc,
+                              title,
+                              cols=1,
+                              footer=NULL,
+                              page_number=NULL,
+                              template_override = NULL) {
+  if (cols == 1) {
+    template_name <- "Title and Content"
+  } else if (cols == 2) {
+    template_name <- "Two Content"
+  } else {
+    stop("In add_generic_slide number of columns must be 1 or 2")
+  }
+  
+  template_name <- if (is.null(template_override)) {template_name} else {template_override}
+  
+  doc <- addSlide(doc, template_name)
+  doc <- addTitle(doc, title)
+  if (!is.null(footer)) {
+    doc <- addFooter(doc, footer)
+  }
+  if (!is.null(page_number)) {
+    doc <- addPageNumber(doc, page_number)
+  }
+  return(doc)
+}
+
+################################################################################
 #' Add a text only slide temporary Power Point file
 #' @param doc a Temporary Power Point file created by create_powerpoint()
 #' @param title The title of the slide
+#' @param cols The number of columns to add must be 1 or 2
 #' @param paragraph Some text
 #' @param footer OPTIONAL a footer for the slide
 #' @param page_number OPTIONAL the page number
@@ -69,10 +111,17 @@ add_title_slide <- function(doc,
 #' @export
 add_text_slide <- function(doc,
                            title,
+                           cols=1,
                            paragraph,
                            footer=NULL,
                            page_number=NULL) {
-  doc <- addSlide(doc, "Title and Content")
+  if (cols == 1) {
+    doc <- addSlide(doc, "Title and Content")
+  } else if (cols == 2) {
+    doc <- addSlide(doc, "Two Content")
+  } else {
+    stop("In add_text_slide number of columns must be 1 or 2")
+  }
   doc <- addTitle(doc, title)
   doc <- addParagraph(doc, paragraph)
   if (!is.null(footer)) {
@@ -88,6 +137,7 @@ add_text_slide <- function(doc,
 #' Add a slide with a data table to a temporary Power Point file
 #' @param doc a Temporary Power Point file created by create_powerpoint()
 #' @param title The title of the slide
+#' @param cols The number of columns to add must be 1 or 2
 #' @param df_table A dataframe to be added as a table
 #' @param paragraph OPTIONAL Some text describing the table
 #' @param footer OPTIONAL a footer for the slide
@@ -98,6 +148,7 @@ add_text_slide <- function(doc,
 #' @export
 add_table_slide <- function(doc,
                             title,
+                            cols=1,
                             df_table,
                             paragraph=NULL,
                             footer=NULL,
@@ -105,10 +156,12 @@ add_table_slide <- function(doc,
                             invert=FALSE) {
   # if we have not been given a paragraph, assume that we're only going to add
   # table with no explantory text
-  if (!is.null(paragraph)) {
+  if (cols == 1) {
+    doc <- addSlide(doc, "Title and Content")
+  } else if (cols == 2) {
     doc <- addSlide(doc, "Two Content")
   } else {
-    doc <- addSlide(doc, "Title and Content")
+    stop("In add_table_slide number of columns must be 1 or 2")
   }
   
   doc <- addTitle(doc, title)
@@ -135,6 +188,7 @@ add_table_slide <- function(doc,
 #' Add a title slide to a temporary Power Point file
 #' @param doc a Temporary Power Point file created by create_powerpoint()
 #' @param title The title of the slide
+#' @param cols The number of columns to add must be 1 or 2
 #' @param ggplot_object A ggplot object
 #' @param paragraph OPTIONAL Some text describing the ggplot_image
 #' @param footer OPTIONAL a footer for the slide
@@ -145,6 +199,7 @@ add_table_slide <- function(doc,
 #' @export
 add_plot_slide <- function(doc,
                            title,
+                           cols=1,
                            ggplot_object,
                            paragraph=NULL,
                            footer=NULL,
@@ -152,10 +207,12 @@ add_plot_slide <- function(doc,
                            invert=FALSE) {
   # if we have not been given a paragraph, assume that we're only going to add
   # plot with no explantory text
-  if (!is.null(paragraph)) {
+  if (cols == 1) {
+    doc <- addSlide(doc, "Title and Content")
+  } else if (cols == 2) {
     doc <- addSlide(doc, "Two Content")
   } else {
-    doc <- addSlide(doc, "Title and Content")
+    stop("In add_plot_slide number of columns must be 1 or 2")
   }
   doc <- addTitle(doc, title)
   if (invert) {
@@ -175,5 +232,57 @@ add_plot_slide <- function(doc,
   if (!is.null(page_number)) {
     doc <- addPageNumber(doc, page_number)
   }
+  return(doc)
+}
+
+################################################################################
+#' Inserts a list into the current slide
+#' @param doc a Temporary Power Point file created by create_powerpoint()
+#' @param list_of_items A vector of items to incldue in list
+#' @param numbered A boolean if true the list is numbered
+#' 
+#' @return the temporary Power Point file with a title slide added
+#' @export
+insert_list <- function(doc, 
+                        list_of_items,
+                        append = FALSE,
+                        numbered = FALSE) {
+  if (numbered) {
+    list_type = 'ordered'
+  } else{ 
+    list_type = 'unordered'
+  }
+  
+  doc <- addParagraph(doc, 
+                      value = list_of_items,
+                      append = append,
+                      par.properties =  parProperties(list.style = list_type))  
+  
+  return(doc)
+}
+
+################################################################################
+#' Inserts a plot directly into the current slide
+#' @param doc a Temporary Power Point file created by create_powerpoint()
+#' @param ggplot_object A vector of items to incldue in list
+#' @param vector plot as vector graphic or png
+#' @param font_size rescales some aspects of the plot
+#' 
+#' @return the temporary Power Point file with a title slide added
+#' @export
+insert_plot <- function(doc, ggplot_object, vector=TRUE, font_size=6) {
+  doc <- addPlot(doc, fun = print, x = ggplot_object, vector.graphic = vector, pointsize = font_size)
+  return(doc)
+}
+
+################################################################################
+#' Inserts a dataframe into the current slide as a table
+#' @param doc a Temporary Power Point file created by create_powerpoint()
+#' @param df_table A data frame to be inserted as a table
+#' 
+#' @return the temporary Power Point file with a title slide added
+#' @export
+insert_table <- function(doc, df_table) {
+  doc <- addFlexTable(doc, FlexTable(df_table))
   return(doc)
 }
