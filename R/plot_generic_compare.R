76915c1df178f5fd55c92cd61c3daf42287c735d
James Thompson <james.thompson@imperial.ac.uk>
2018-05-22 09:57:38 +0100

    Add functionality for cumulative plots and different plotting packages

--- R/plot_generic_compare.R
+++ R/plot_generic_compare.R
@@ -9,30 +9,43 @@
 #'                be used to determine the x axis
 #' @param discriminent A string of the column name in dat for the variable that
 #'                     be used to determine the block of the bar chart
+#' @param cum_plot Generate a cumulative plot, this is ignored if compare is not
+#'                 'year'
+#' @param plot_mode What package will be used to produce the plot, options:
+#'                  ggplot, bokeh, highcharter
 #'
-#' @return A ggplot object
+#' @return A list containt p = ggplot object, d = the data frame used in the plot
 #' @export
-plot_generic_compare <- function(dat, params, compare, disciminent) {
+plot_generic_compare <- function(dat, params, compare, disciminent, cum_plot=FALSE, plot_mode="ggplot") {
   dat$country_name = sapply(dat$country_name, shorten_name, USE.NAMES = FALSE)
   no_disc <- is.null(disciminent)
   
   if (no_disc) {
     disciminent <- compare # we set this 
   }
-
+  
+  dat <- filter_by_params(dat, params)
+  
   dat$outcome <- dat[, params$metric]
   dat$compare <- dat[, compare]
   dat$disc    <- dat[, disciminent]
   
   ordered_compare <- is.numeric(dat$compare)
-
+  
   dat <- filter_by_params(dat, params)
-
+  
   dat <- dat %>%
     group_by(compare, disc) %>%
     summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
     arrange(desc(outcome))
-
+  
+  if (cum_plot && compare == "year") {
+    dat <- dat %>%
+      group_by(disc) %>%
+      arrange(compare) %>% # note at this point compare is year, so this sorts by year
+      mutate(outcome = cumsum(outcome))
+  }
+  
   # find the top n_plot compares by outcome
   if (ordered_compare) {
     df_top_compares <- dat %>%
@@ -45,12 +58,14 @@ plot_generic_compare <- function(dat, params, compare, disciminent) {
       summarize(outcome = sum(outcome, na.rm = TRUE)) %>%
       arrange(desc(outcome))   
   }
-               
+
+  rot_angle <- if (compare == "year" || length(unique(dat$compare)) <= 5) {0} else {90}
+  
   units <- graph_num_div(max(df_top_compares$outcome), params$metric)
   dat$outcome <- dat$outcome / units$numdiv
   df_top_compares$outcome <- df_top_compares$outcome / units$numdiv
   top_label_shift <- max(df_top_compares$outcome) / 50
-
+  
   top_compares <- df_top_compares$compare
   if (!is.null(params$n_plot)) {
     if (params$n_plot < length(top_compares)) {
@@ -68,26 +83,39 @@ plot_generic_compare <- function(dat, params, compare, disciminent) {
     factor(dat$compare, levels = top_compares)
   
   my_cols <- get_palette(disciminent, unique(dat$disc))
-
+  
   dat$compare <- as.factor(dat$compare)
   df_top_compares$compare <- as.factor(df_top_compares$compare)
-
-  ggplot(dat, aes(x = compare,
-                  y = outcome,
-                  fill = factor(disc))) +
-    geom_bar(stat = "identity", color = "black") + 
-    scale_fill_manual(values = my_cols) +
-    theme_bw() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.text.x = element_text(angle = 90, hjust = 1),
-          legend.title = element_blank()) +
-    xlab("") + 
-    ylab(units$ylabscale) + 
-    annotate("text", x = df_top_compares$compare, 
-                     y = df_top_compares$outcome + top_label_shift,
-                     label = as.character(signif(df_top_compares$outcome, 2)),
-                     colour = "black", size = 3) +
-    
-    ggtitle(params$title)
+  
+  p <- NULL
+  if (plot_mode == "ggplot") {
+    p <- ggplot(dat, aes(x = compare,
+                         y = outcome,
+                         fill = factor(disc))) +
+      geom_bar(stat = "identity", color = "black") + 
+      scale_fill_manual(values = my_cols) +
+      theme_bw() +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.text.x = element_text(angle = rot_angle, hjust = 0.5),
+            legend.title = element_blank()) +
+      xlab("") + 
+      ylab(units$ylabscale) + 
+      annotate("text", x = df_top_compares$compare, 
+               y = df_top_compares$outcome + top_label_shift,
+               label = as.character(signif(df_top_compares$outcome, 2)),
+               colour = "black", size = 3) +
+      
+      ggtitle(params$title)
+  }
+  if (plot_mode == "bokeh") {
+    p <- figure(title = params$title) %>%
+      ly_bar(compare, outcome, data = dat, color = disc, hover = TRUE)
+  }
+  if (plot_mode == "highcharter") {
+    p <- highchart(dat, "column", hcaes(x = compare, y = outcome))
+  }
+  
+  
+  return(list(p = p, d = dat))
 }
